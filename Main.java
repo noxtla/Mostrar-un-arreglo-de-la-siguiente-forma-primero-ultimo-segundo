582a83140d3a11f016213af8f7f4b41887441964
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-02-19 11:52:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+package arreglo;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in);
+		int arreglo [] = new int [10];
+
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("Ingresa el numero para la pos " + (i+1));
+			arreglo[i] = entrada.nextInt();
+		}
+		
+		
+		System.out.println("Mostrando");
+		for(int i = 0; i<5; i++) {
+			System.out.println(arreglo[i] + " ");
+			System.out.println(arreglo[9-i] + " ");
+		}
+	}
+
+}
